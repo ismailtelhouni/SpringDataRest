e13fa4b6f053b6511e8d3c940a658cfb96b80824
ismailtelhouni <ismailtelhouni123@gmail.com>
2024-10-20 16:38:55 +0100

    add docker compose file and dockerize backend

--- src/main/java/org/spring/data/rest/config/EnvConfig.java
+++ src/main/java/org/spring/data/rest/config/EnvConfig.java
@@ -1,13 +1,13 @@
-package org.spring.data.rest.config;
-
-import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class EnvConfig {
-    @Bean
-    public Dotenv dotenv() {
-        return Dotenv.load();
-    }
-}
+//package org.spring.data.rest.config;
+//
+//import io.github.cdimascio.dotenv.Dotenv;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class EnvConfig {
+//    @Bean
+//    public Dotenv dotenv() {
+//        return Dotenv.load();
+//    }
+//}
